2ce390117de455010dde3cf107a395c7c805c50f
Kyle Vandenbossche <kylevandenbossche@gmail.com>
2023-06-22 19:45:23 -0400

    added icon nav bar in app-media query mode

--- fridge-app/frontend/src/components/Ingredients/IngredientNav.tsx
+++ fridge-app/frontend/src/components/Ingredients/IngredientNav.tsx
@@ -4,22 +4,11 @@ import FruitGrid from "./FruitGrid";
 import ProteinGrid from "./ProteinGrid";
 import SeasoningsGrid from "./SeasoningsGrid";
 import { useRef } from "react";
+import "./IngredientNav.css"
+
 
 
 export function IngredientNav () {
-
-    const proteins = useRef(null);
-    const fruit = useRef(null);
-    const carbs = useRef(null);
-    const seasonings = useRef(null);
-
-    const scrollToSection = (elementRef: any) => {
-        window.scrollTo({
-            top: elementRef.current.offsetTop,
-            behavior: 'smooth'
-
-        })
-    }
 
 
 
@@ -29,7 +18,17 @@ export function IngredientNav () {
             <li className="link">Proteins</li>
             <li className="link">Fruit</li>
             <li className="link">Carbs</li>
+            <li className="link">Vegetables</li>
             <li className="link">Seasonings</li>
+
+
+            <div className="icon-container">
+                <li className="picture-link"><img src={ require('./PicturesNav/Proteins.PNG')} height="50" /></li>
+                <li className="picture-link"><img src={ require('./PicturesNav/Fruit.PNG')} height="50" /></li>
+                <li className="picture-link"><img src={ require('./PicturesNav/Carbohydrates.PNG')} height="50" /></li>
+                <li className="picture-link"><img src={ require('./PicturesNav/Seasonings.PNG')} height="50" /></li>
+                <li className="picture-link"><img src={ require('./PicturesNav/Vegetables.PNG')} height="50" /></li>
+            </div>
 
         </ul>
 
