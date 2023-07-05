3673319c5f58b93c4b41670eef1f10be93b87d32
Kyle Vandenbossche <kylevandenbossche@gmail.com>
2023-07-05 19:16:20 -0400

    readded home logo

--- fridge-app/frontend/src/components/SignIn.tsx
+++ fridge-app/frontend/src/components/SignIn.tsx
@@ -7,11 +7,14 @@ const SignIn = () => {
   const { user } = useContext(AuthContext);
   return (
     <>
-      <h2>Fridge to Table</h2>
+      <div className="home-page-div">
+      <img className='fridge-to-table-icon' src={ require('./fridge-to-table.PNG')} height="319" />
       <div className="card">
+        
         <button className="sign-in-button" onClick={signInWithGoogle}>
           Sign in with Google<span className="google-icon"></span>
         </button>
+      </div>
       </div>
     </>
   );
